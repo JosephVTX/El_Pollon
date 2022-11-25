e5ccfa894880fd86b7b529f9fc9de1bc0d55c39e
Joseph VTX <1395609@senati.pe>
2022-11-25 15:39:52 -0500

    semifinish v2

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,20 @@
+
+(() => {
+
+    const pathname = window.location.pathname.split('/').pop();
+    let menuItem = document.querySelector(`.menu__item[href='${pathname}']`);
+
+    menuItem.classList.add('menu__item--active');
+
+
+})()
+
 const btnMenu = document.querySelector('#btnMenu');
 
 
 
-btnMenu.addEventListener('click', ()=>{
+
+btnMenu.addEventListener('click', () => {
 
     let menu = document.querySelector('.nav__menu');
 
